2c678bf59982e28d211d3932c5957d9e8b76ecf0
pneugebala <pneugebala@gmail.com>
2019-01-24 18:11:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+if [[ -n $1 ]]; then
+  sudo apt-get update
+  sudo apt-get install -y $1
+fi
+
 chmod +x gradlew
 WORK_DIR=$PWD
 
-cd "$1"
-sh -c "${WORK_DIR}/gradlew ${@:2}"
+cd "$2"
+sh -c "${WORK_DIR}/gradlew ${@:3}"
